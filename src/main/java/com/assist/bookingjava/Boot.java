4ee5be17a28613acb27802022293dfb8428ecf44
tatarudimitrie <intern.dimitrie.tataru@assist.ro>
2017-07-11 17:05:29 +0300

    First commit

--- src/main/java/com/assist/bookingjava/Boot.java
+++ src/main/java/com/assist/bookingjava/Boot.java
@@ -3,9 +3,6 @@ package com.assist.bookingjava;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-/**
- * Created by frunza on 07.07.2017.
- */
 @SpringBootApplication
 public class Boot {
 
